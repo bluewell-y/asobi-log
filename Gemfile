13e8030697ee1c3f49b78a0508013eccd6d610a5
bluewell-y <aoiy04414@gmail.com>
2026-08-29 07:09:57 +0900

    モデルの基本テストを追加
    - User/Place/Favorite/Visitモデルのバリデーション・authenticateのテストを実装
    - minitestをRails 7.1と互換性のある5系に固定
    - fixturesのemail重複・user_id不足を修正

--- Gemfile
+++ Gemfile
@@ -61,6 +61,9 @@ group :development do
 end
 
 group :test do
+  # Rails 7.1はminitest 6系と非互換のため、5系に固定
+  gem "minitest", "~> 5.25"
+
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
   gem "selenium-webdriver"
--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_place
+    Place.new(
+      name: "テスト公園",
+      address: "東京都渋谷区1-1-1",
+      user: users(:one)
+    )
+  end
+
+  test "name, address, userがあれば保存できる" do
+    assert valid_place.valid?
+  end
+
+  test "nameが空だと保存できない" do
+    place = valid_place
+    place.name = ""
+    assert_not place.valid?
+    assert_includes place.errors[:name], "can't be blank"
+  end
+
+  test "addressが空だと保存できない" do
+    place = valid_place
+    place.address = ""
+    assert_not place.valid?
+    assert_includes place.errors[:address], "can't be blank"
+  end
+
+  test "userが無いと保存できない" do
+    place = valid_place
+    place.user = nil
+    assert_not place.valid?
+  end
 end
