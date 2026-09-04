305a91f8f809b0837262a7a376705cb0375ece9a
bluewell-y <aoiy04414@gmail.com>
2026-09-05 07:54:53 +0900

    画像のリサイズにImageMagickを使えるようにする
    - image_processing gemとmini_magick gemを追加
    - config.active_storage.variant_processorを:mini_magickに設定（この環境には libvips が無く ImageMagick のみ入っているため）

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module AsobiLog
     # エラーメッセージ等を日本語で表示する
     config.i18n.default_locale = :ja
 
+    # 画像のリサイズに ImageMagick（mini_magick）を使う
+    config.active_storage.variant_processor = :mini_magick
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
