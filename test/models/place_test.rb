73437a367d419dfb3f5ca626767b8fa14fa84eeb
bluewell-y <aoiy04414@gmail.com>
2026-09-06 14:27:32 +0900

    モデルテストの失敗を修正する
    - エラーメッセージ文言（英語）の比較をerrors.of_kind?に置き換え、i18nを:jaにした影響で落ちていたplace/userのテストを修正。
    - Placeのトップ画像必須化に合わせ、valid_placeでcover_imageを添付。テスト用画像test/fixtures/files/test_cover.pngを追加。
    - cover_imageが無いと保存できないことのテストを追加。

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,15 +1,21 @@
 require "test_helper"
 
 class PlaceTest < ActiveSupport::TestCase
-  def valid_place
-    Place.new(
+  def valid_place                                                                                         
+    place = Place.new(
       name: "テスト公園",
       address: "東京都渋谷区1-1-1",
       user: users(:one)
+    )                                                                                                     
+    place.cover_image.attach(
+      io: file_fixture("test_cover.png").open,
+      filename: "test_cover.png",
+      content_type: "image/png"
     )
+    place
   end
-
-  test "name, address, userがあれば保存できる" do
+                                                                                                          
+  test "name, address, user, cover_imageがあれば保存できる" do
     assert valid_place.valid?
   end
 
@@ -17,19 +23,25 @@ class PlaceTest < ActiveSupport::TestCase
     place = valid_place
     place.name = ""
     assert_not place.valid?
-    assert_includes place.errors[:name], "can't be blank"
+    assert place.errors.of_kind?(:name, :blank)
   end
 
-  test "addressが空だと保存できない" do
+  test "addressが空だと保存できない" do                                                                   
     place = valid_place
     place.address = ""
     assert_not place.valid?
-    assert_includes place.errors[:address], "can't be blank"
+    assert place.errors.of_kind?(:address, :blank)
+  end
+
+  test "cover_imageが無いと保存できない" do
+    place = Place.new(name: "テスト公園", address: "東京都渋谷区1-1-1", user: users(:one))
+    assert_not place.valid?                                                                               
+    assert place.errors.of_kind?(:cover_image, :blank)
   end
 
   test "userが無いと保存できない" do
     place = valid_place
-    place.user = nil
+    place.user = nil                                                                                      
     assert_not place.valid?
   end
 end
